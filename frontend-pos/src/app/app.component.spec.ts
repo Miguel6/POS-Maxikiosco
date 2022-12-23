9a2b2eb39a759fe70df44ca827484bede49acb3e
Miguel Angel Amengual <amengualma6@gmail.com>
2022-12-23 18:03:28 -0300

    Added styles

--- frontend-pos/src/app/app.component.spec.ts
+++ frontend-pos/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'frontend-pos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-pos');
+    expect('').toEqual('frontend-pos');
   });
 
   it('should render title', () => {
