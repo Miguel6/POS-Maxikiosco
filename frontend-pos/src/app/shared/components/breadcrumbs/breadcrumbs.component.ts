51f21c5fb8fcd032a859f6a101f692f348567ffa
Miguel Angel Amengual <amengualma6@gmail.com>
2023-01-10 20:58:16 -0300

    Added unsuscription onDestroy breadcrumb.

--- frontend-pos/src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ frontend-pos/src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,25 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {BreadcrumbService} from '../../services/breadcrumb.service';
 import {BreadcrumbEntryConfig, CustomRouteData} from '../../../app-routing.module';
 import {PathName} from '../../models/path-name';
 import {NavigatorHelper} from '../../providers/navigator-helper';
 import {Path} from '../../constants/path';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'pos-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss']
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
   public activePathName: string;
   public breadcrumbs: PathName[];
 
+  private readonly breadcrumbServiceSuscription: Subscription;
+
   constructor(private breadcrumbService: BreadcrumbService,
               private navigatorHelper: NavigatorHelper) {
-    this.breadcrumbService.getObservable().subscribe(data => {
+    this.breadcrumbServiceSuscription = this.breadcrumbService.getObservable().subscribe(data => {
       this.activePathName = this.getActivePath(data);
       this.breadcrumbs = this.mapBreadcrumbs(data.breadcrumb ?? []);
     })
+  }
+
+  ngOnDestroy() {
+    this.breadcrumbServiceSuscription?.unsubscribe();
   }
 
   private mapBreadcrumbs(breadcrumbs: BreadcrumbEntryConfig[]): PathName[] {
