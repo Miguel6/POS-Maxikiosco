0fab5828add83bd0d0d32ef58d29119c7f3ae14f
Miguel Angel Amengual <amengualma6@gmail.com>
2023-02-10 11:37:05 -0300

    Added category ALL

--- frontend-pos/src/app/features/products/category-list/category-list.component.ts
+++ frontend-pos/src/app/features/products/category-list/category-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Category} from '../models/category';
 
 @Component({
@@ -9,9 +9,20 @@ import {Category} from '../models/category';
 export class CategoryListComponent {
 
   @Input() categories: Category[];
+  @Input() showCategoryAll: boolean = false;
+
+  @Output() onClickAll: EventEmitter<void> = new EventEmitter<void>();
+  @Output() onClickCategory: EventEmitter<Category> = new EventEmitter<Category>();
+
+
   public activeCategory: number = 0;
 
   public clickOnCategory(id: number): void {
     this.activeCategory = id;
+    this.onClickCategory.emit(this.categories.find(c => c.id == this.activeCategory))
+  }
+
+  public clickOnCategoryAll(): void {
+    this.onClickAll.emit();
   }
 }
