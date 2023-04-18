1e7dee35fbe4fb383f98d9fe4893c1f3f91d146c
Miguel Angel Amengual <amengualma6@gmail.com>
2023-04-18 16:23:55 -0300

    Edited comma per period and the spacing between digits

--- frontend-pos/src/app/app.module.ts
+++ frontend-pos/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -10,6 +10,10 @@ import {SettingsModule} from './features/settings/settings.module';
 import {StatisticsModule} from './features/statistics/statistics.module';
 import {UsersModule} from './features/users/users.module';
 import {HomeModule} from './features/home/home.module';
+import localeEsAr from '@angular/common/locales/es-AR';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localeEsAr, 'es-Ar');
 
 @NgModule({
   declarations: [
@@ -27,7 +31,7 @@ import {HomeModule} from './features/home/home.module';
     StatisticsModule,
     UsersModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'es-Ar'}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
